6a90c92e2b2ea026a4718fbd0487ded01f41a3eb
刘显志 <1010465852@qq.com>
2020-12-07 18:01:40 +0800

    feat: 排序算法

---
+++ 排序/插入排序.js
@@ -0,0 +1,18 @@
+// 思想
+// 将左侧序列看成一个有序序列，每次将一个数字插入该有序序列。
+// 插入时，从有序序列最右侧开始比较，若比较的数较大，后移一位。
+
+function insertSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    let target = i;
+    for (let j = i - 1; j >= 0; j--) {
+      if (array[target] < array[j]) {
+        [array[target], array[j]] = [array[j], array[target]];
+        target = j;
+      } else {
+        break;
+      }
+    }
+  }
+  return array;
+}
