d5d0a4248cadb62cb805b75854ffffd4f0a7c96e
刘显志 <1010465852@qq.com>
2021-01-06 19:57:55 +0800

    fix:链表-环形链表O(1)内存

--- leetcode/141.环形链表.js
+++ leetcode/141.环形链表.js
@@ -18,14 +18,26 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-    let set = new Set();
-    let p = head
-    while (p) {
-      if (set.has(p)) return true;
-      set.add(p);
-      p = p.next;
+    let dummyHead = new ListNode();
+    dummyHead.next = head;
+    let fast = slow = dummyHead
+    if (fast.next == null || fast.next.next == null) return false;
+    while (fast && fast.next) {
+      fast = fast.next.next;
+      slow = slow.next;
+      if (fast == slow) return true
     }
-    return false;
+    return false
 };
 // @lc code=end
-
+//O(n)内存
+var hasCycle = function(head) {
+  let set = new Set();
+  let p = head
+  while (p) {
+    if (set.has(p)) return true;
+    set.add(p);
+    p = p.next;
+  }
+  return false;
+};
